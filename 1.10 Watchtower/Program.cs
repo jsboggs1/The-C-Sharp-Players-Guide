439cc013425d83f10e3587410d8998e0c7a644ce
jsboggs1 <133532611+jsboggs1@users.noreply.github.com>
2023-08-25 11:40:00 -0500

    Add files via upload

---
+++ 1.10 Watchtower/Program.cs
@@ -0,0 +1,70 @@
+﻿// ask for an x and y value and use if statements to deterimine the direction the enemy is coming from
+
+int x;
+int y;
+
+Console.Write("X coordinate? "); // obtain x coord from user and convert to int
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y coordinate? "); // same as x coord step
+y = Convert.ToInt32(Console.ReadLine());
+
+if (x < 0) // could also write these as if (x < 0 && y > 0) individually
+{
+    Console.ForegroundColor = ConsoleColor.Green; // change color of enemy direction announcement
+
+    if (y > 0)
+    {
+        Console.Write("The enemy is to the Northwest!");
+    }
+    if (y == 0)
+    {
+        Console.Write("The enemy is to the West!");
+    }
+    if (y < 0)
+    {
+        Console.Write("The enemy is to the Southwest!");
+    }
+    Console.ForegroundColor = ConsoleColor.Gray; // change the color back to normal after direction announcement
+}
+
+else if (x == 0) // if x < 0 is false, move on
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    if (y > 0)
+    {
+        Console.Write("The enemy is to the North!");
+    }
+    if (y == 0)
+    {
+        Console.Write("They're right on top of us!");
+    }
+    if (y < 0)
+    {
+        Console.Write("The enemy is to the South!");
+    }
+
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
+
+else if (x > 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    if (y > 0)
+    {
+        Console.Write("The enemy is to the Northeast!");
+    }
+    if (y == 0)
+    {
+        Console.Write("The enemy is to the East!");
+    }
+    if (y < 0)
+    {
+        Console.Write("The enemy is to the Southeast!");
+    }
+
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
+
---
+++ 1.11 Buying-Inventory/Program.cs
@@ -0,0 +1,52 @@
+﻿// setting up inventory choices, book isn't as clear on this one, the web soltion clears it up though
+
+Console.WriteLine("The following items are available: \n");
+Console.WriteLine("1 - Rope");
+Console.WriteLine("2 - Torches");
+Console.WriteLine("3 - Climbing Equipment");
+Console.WriteLine("4 - Clean Water");
+Console.WriteLine("5 - Machete");
+Console.WriteLine("6 - Canoe");
+Console.WriteLine("7 - Food Supplies");
+
+// asking the user for a number choice from the list
+Console.Write("What number do you want to see the price of? ");
+
+// converting the user's response to an int
+int itemNumber  = Convert.ToInt32(Console.ReadLine()); 
+
+// using a switch to assign an the number entered to a string value
+string item = itemNumber switch 
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies",
+    _ => "Whatever that is, it isn't on the menu."
+};
+
+// using the string value and then assigning an int value
+int itemPrice = item switch 
+{
+    "Rope" => 10,
+    "Torches" => 16,
+    "Climbing Equipment" => 24,
+    "Clean Water" => 2,
+    "Machete" => 20,
+    "Canoe" => 200,
+    "Food Supplies" => 2,
+};
+
+Console.Write("What is your name? ");
+string name = Console.ReadLine();
+
+// if the person's name is Jerry, they get a 50% discount
+if (name == "Jerry")
+{
+    itemPrice = itemPrice / 2;
+}
+
+Console.WriteLine($"{item} costs {itemPrice} gold.");
