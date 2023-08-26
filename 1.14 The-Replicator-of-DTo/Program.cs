bbd0f091c1af2d26e6722809a2977dfbdb705135
jsboggs1 <133532611+jsboggs1@users.noreply.github.com>
2023-08-26 13:26:05 -0500

    Add files via upload

---
+++ 1.14 The-Replicator-of-DTo/Program.cs
@@ -0,0 +1,36 @@
+﻿// this creates a new array with 5 values, all at 0 because I didn't assign specific values
+int[] array1 = new int[5];
+
+// collecting 5 array values from the user
+Console.WriteLine("Please enter 5 numbers. ");
+array1 [0] = Convert.ToInt32(Console.ReadLine());
+array1 [1] = Convert.ToInt32(Console.ReadLine());
+array1 [2] = Convert.ToInt32(Console.ReadLine());
+array1 [3] = Convert.ToInt32(Console.ReadLine());
+array1 [4] = Convert.ToInt32(Console.ReadLine());
+
+// creating a second array
+int[] array2 = new int[5];
+
+// copying the index of array 1 into array 2
+// index starts at 0, so it copies the values out of array1 starting at 0 until it reaches the length of
+// the original array
+for (int index = 0; index < 5; index++) 
+{
+    array2[index] = array1[index];
+}
+
+// line separator 
+Console.WriteLine("---------------------");
+
+// display array 1 contents in green
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Array 1: {array1[0]}, {array1[1]}, {array1[2]}, {array1[3]}, {array1[4]}");
+Console.ForegroundColor = ConsoleColor.Gray;
+
+// display array 2 contents in cyan
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($"Array 2: {array2[0]}, {array2[1]}, {array2[2]}, {array2[3]}, {array2[4]}");
+Console.ForegroundColor = ConsoleColor.Gray;
+
+
---
+++ 1.15 The-Laws-of-Freach/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 }; // from book
+
+int currentSmallest = int.MaxValue; // from book
+
+/* this is the original loop code from the book
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index] < currentSmallest)
+        currentSmallest = array[index];
+}
+Console.WriteLine(currentSmallest);
+*/
+
+foreach (int value in array) // for every spot in array
+{
+    if (value < currentSmallest) // if the int value is less than the currentSmallest, that's the new smallest value
+    {
+        currentSmallest = value;
+    }
+}
+Console.WriteLine(currentSmallest);
+
+
+
+int[] array2 = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 }; // from book
+
+int total = 0; // from book
+
+/* original loop from book
+for (int index = 0; index < array.Length; index++)
+    total += array2[index];
+float average = (float)total / array.Length;
+Console.WriteLine(average);
+*/
+
+foreach (int value in array2) // for every value in array 2
+{
+    total += value; // add every int value to the total, total = value + total, 
+}
+float average = (float)total / (float)array2.Length; // divide total of array by the length of the array
+Console.WriteLine(average);
