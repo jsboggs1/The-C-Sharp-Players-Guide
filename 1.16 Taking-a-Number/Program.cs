887fe8da676221831336efa186a1a959815235b7
jsboggs1 <133532611+jsboggs1@users.noreply.github.com>
2023-09-01 12:26:44 -0500

    Add files via upload

---
+++ 1.16 Taking-a-Number/Program.cs
@@ -0,0 +1,33 @@
+﻿// creation of a method
+// a method is a repeatable block of code that executes when called
+
+// the "int" in front of the method name tells what value it will be returning. Use "void" if a method doesn't
+// need to return a value.
+
+int AskForNumber(string text) // parameters within the parentheses set up the format of future method calls
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result; // return exits the method
+}
+
+// calling the AskForNumber method, uses the "text" string parameter shown before
+
+// methods GET THEIR OWN VARIABLES, this is why we can use the "result" variable again
+int result = AskForNumber("What is the airspeed velocity of an unladen swallow?"); 
+
+int AskForNumberInRange (string text, int min, int max)
+{
+    Console.WriteLine (text);
+    int numberInRange = Convert.ToInt32(Console.ReadLine());
+
+    while (numberInRange < min || numberInRange > max)
+    {
+        Console.WriteLine("Please enter a number within the range of values given.");
+        numberInRange = Convert.ToInt32(Console.ReadLine());
+    }
+    return numberInRange;
+}
+
+// calling the AskForNumberInRange method
+int numberInRange = AskForNumberInRange("Give me a number between 1 and 10.", 1, 10);
---
+++ 1.17 Countdown/Program.cs
@@ -0,0 +1,21 @@
+﻿// write a program that counts down from 10 to 1 using recursion (a method calling itself)
+
+// for recursion you need to have a "base case," something that stops the recursion from repeating infinitely
+
+int CountDown (int startNumber)
+{
+    if (startNumber == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        // this could be written as startNumber = 10 - 1 and then calling the method to continue running
+        // until startNumber reaches 1
+
+        Console.WriteLine(startNumber);
+        return startNumber = CountDown(startNumber - 1); 
+    }
+}
+
+int startNumber = CountDown (10); // setting the startNumber to be equal to 10 so the method will count down 
