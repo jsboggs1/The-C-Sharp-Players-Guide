0438f9aaef126ddd849cd7fa27bbc1bbe4d3dd9e
jsboggs1 <133532611+jsboggs1@users.noreply.github.com>
2023-09-14 11:44:15 -0500

    Add files via upload

---
+++ 1.19 Simulas-Test/Program.cs
@@ -0,0 +1,28 @@
+﻿// using a variable to store the current chest state
+ChestState current = ChestState.locked;
+
+while (true)
+{
+    Console.Write($"The chest is {current}. What do you want to do? ");
+    string answer = Console.ReadLine();
+
+    if (answer == "unlock" && current == ChestState.locked)
+    {
+        current = ChestState.unlocked;
+    }
+    if (answer == "open" && current == ChestState.unlocked)
+    {
+        current = ChestState.opened;
+    }
+    if (answer == "close" && current == ChestState.opened)
+    {
+        current = ChestState.unlocked;
+    }
+    if (answer == "lock" && current == ChestState.unlocked)
+    {
+        current = ChestState.locked;
+    }
+}
+
+// enumeration defining the state of the chest
+enum ChestState { locked, unlocked, opened }
